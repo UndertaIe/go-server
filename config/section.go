b3d13dab09ee7e32bdcedacbbba731087b8fbe8b
root <root@localhost.localdomain>
2022-08-15 12:22:24 +0800

    add config

---
+++ config/section.go
@@ -0,0 +1,79 @@
+package config
+
+import (
+	"time"
+)
+
+type ServerSetting struct {
+	RunMode      string
+	HttpPort     string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+type AppSetting struct {
+	DefaultPageSize       int
+	MaxPageSize           int
+	DefaultContextTimeout time.Duration
+	LogSavePath           string
+	LogFileName           string
+	LogFileExt            string
+	UploadSavePath        string
+	UploadServerUrl       string
+	UploadImageMaxSize    int
+	UploadImageAllowExts  []string
+}
+
+type EmailSetting struct {
+	Host     string
+	Port     int
+	UserName string
+	Password string
+	IsSSL    bool
+	From     string
+	To       []string
+}
+
+type JWTSetting struct {
+	Secret string
+	Issuer string
+	Expire time.Duration
+}
+
+type DatabaseSetting struct {
+	DBType       string
+	UserName     string
+	Password     string
+	Host         string
+	DBName       string
+	TablePrefix  string
+	Charset      string
+	ParseTime    bool
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+var sections = make(map[string]interface{})
+
+func (s *Setting) ReadSection(k string, v interface{}) error {
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := sections[k]; !ok {
+		sections[k] = v
+	}
+	return nil
+}
+
+func (s *Setting) ReloadAllSection() error {
+	for k, v := range sections {
+		err := s.ReadSection(k, v)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ config/settings.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+func NewDBSetting() DatabaseSetting {
+	return DatabaseSetting{}
+}
+
+type Setting struct {
+	vp *viper.Viper // 对配置读取模块自定义封装
+}
+
+func NewSetting(configs ...string) (*Setting, error) {
+	vp := viper.New()
+	vp.SetConfigName("config")
+	for _, config := range configs {
+		if config != "" {
+			vp.AddConfigPath(config)
+		}
+	}
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Setting{vp}
+	s.WatchSettingChange()
+	return s, nil
+}
+
+func (s *Setting) WatchSettingChange() {
+	go func() {
+		s.vp.WatchConfig()
+		s.vp.OnConfigChange(func(in fsnotify.Event) {
+			_ = s.ReloadAllSection()
+		})
+	}()
+}
