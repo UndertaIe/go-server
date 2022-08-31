b7d663dbcb1f91040cf900c9e7315b582ba88905
UndertaIe <n604712799@gmail.com>
2022-08-31 20:06:28 +0800

    随机n个字符串函数

--- pkg/utils/random.go
+++ pkg/utils/random.go
@@ -1,1 +1,22 @@
 package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+var ASCII = []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func GetRandomString(n int) string {
+	result := make([]byte, n)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	l := len(ASCII)
+	for i := 0; i < n; i++ {
+		result = append(result, ASCII[r.Intn(l)])
+	}
+	return string(result)
+}
+
+func GetRandomNum(n int) string {
+	return ""
+}
