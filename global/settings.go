351869156a2035b37b502a81ce3544e173643618
UndertaIe <n604712799@gmail.com>
2022-09-02 00:48:49 +0800

    添加了全局变量，实现了JWT接口方法

--- global/settings.go
+++ global/settings.go
@@ -1,6 +1,8 @@
 package global
 
 import (
+	"time"
+
 	"github.com/UndertaIe/passwd/config"
 )
 
@@ -10,4 +12,24 @@ var (
 	ServerSettings   *config.ServerSetting
 	EmailSettings    *config.EmailSetting
 	SmsSettings      *config.SmsSetting
+	JwtSettings      *config.JWTSetting
 )
+
+type Globals struct{}
+
+func NewGlobal() *Globals {
+	return &Globals{}
+}
+
+func (g Globals) GetJWTSecret() []byte {
+	return []byte(JwtSettings.Secret)
+}
+
+func (g Globals) GetJWTIssuer() string {
+	return JwtSettings.Issuer
+}
+
+func (g Globals) GetJWTExpireTime() int64 {
+	now := time.Now()
+	return now.Add(JwtSettings.Expire).Unix()
+}
