e9db5974608cab4e1809d169433ba194f4b4ceef
UndertaIe <n604712799@gmail.com>
2022-09-02 10:53:26 +0800

    生成随机字符串功能

--- pkg/utils/random.go
+++ pkg/utils/random.go
@@ -5,18 +5,23 @@ import (
 	"time"
 )
 
-var ASCII = []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+type Type string
 
-func GetRandomString(n int) string {
+const (
+	// 随机字符串包含: 0-9，a-z,A-Z
+	CHARS Type = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	// 字母随机字符串包含: a-z,A-Z
+	LETTERS Type = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	// 数字随机字符串包含: 0-9
+	NUMS Type = "0123456789"
+)
+
+func GetRandomString(str Type, n int) string {
 	result := make([]byte, n)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	l := len(ASCII)
+	l := len(str)
 	for i := 0; i < n; i++ {
-		result = append(result, ASCII[r.Intn(l)])
+		result = append(result, str[r.Intn(l)])
 	}
 	return string(result)
-}
-
-func GetRandomNum(n int) string {
-	return ""
 }
