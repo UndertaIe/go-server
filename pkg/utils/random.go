821bd44754074f05bf43f40f3630acd989d5d29f
UndertaIe <n604712799@gmail.com>
2022-09-02 23:02:53 +0800

    修改了随机字符串类型

--- pkg/utils/random.go
+++ pkg/utils/random.go
@@ -5,19 +5,20 @@ import (
 	"time"
 )
 
-type Type string
+// 字符类型
+type CharType string
 
 const (
 	// 随机字符串包含: 0-9，a-z,A-Z
-	CHARS Type = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	CHARS CharType = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	// 字母随机字符串包含: a-z,A-Z
-	LETTERS Type = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	LETTERS CharType = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	// 数字随机字符串包含: 0-9
-	NUMS Type = "0123456789"
+	NUMS CharType = "0123456789"
 )
 
-func GetRandomString(str Type, n int) string {
-	result := make([]byte, n)
+func GetRandomString(str CharType, n int) string {
+	result := make([]byte, 0, n) // len=0, cap=n的切片
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	l := len(str)
 	for i := 0; i < n; i++ {
