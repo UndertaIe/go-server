d620fdfcdde7300c65baba53d44bbed0025c30f6
UndertaIe <n604712799@gmail.com>
2022-09-06 21:43:21 +0800

    update

--- pkg/app/response.go
+++ pkg/app/response.go
@@ -29,12 +29,10 @@ func (resp *Response) To(data interface{}) {
 }
 
 func (resp *Response) ToList(x interface{}, pager *page.Pager) {
-	t := reflect.TypeOf(x)
-	if t.Kind() != reflect.Slice { // 判断是否是切片
+	v := reflect.ValueOf(x)
+	if v.Kind() != reflect.Slice { // 判断是否是切片
 		panic("x type must be slice")
 	}
-
-	v := reflect.ValueOf(x) // 用于遍历切片
 	data := make([]any, 0)
 	len := v.Len()
 	for i := 0; i < len; i++ {
